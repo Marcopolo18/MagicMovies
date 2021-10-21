a454d9d005b04ab6bfc1da284b4cb719abd0d268
Marcopolo18 <82804068+Marcopolo18@users.noreply.github.com>
2021-10-21 16:57:50 +0200

    3rd commit with article/editor con

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -2,9 +2,6 @@
 @section('title', 'Message List')
 @section('content')
 
-
-
-
 <div class="py-12">
     <h2>Create new post: </h2>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -19,30 +16,72 @@
                     <input class="inputs" type="file" name="file_path" placeholder="Image" autocomplete="off"><br>
 
                     @csrf
-                    <button id="create" class="inline-flex disabled items-center px-4 py-2 mx-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">Submit</button>
+                    <button id="newpost" class="inline-flex disabled items-center px-4 py-2 mx-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">Submit</button>
                 </form>
             </div>
         </div>
     </div>
 </div>
 
+
+
 <div class="py-12">
+    <h2>View/delete posts: </h2>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
                 <div id="showposts">
                     <ul>
                         @foreach ($posts as $post)
+                        <li><b><a href="/article/{{$post->id}}">{{$post->title}}:</a></b></li>
                         <li>
-                        {{$post->file_path}}<br>
-                        <b>{{$post->title}}:</b><br>
-                        {{$post->category}}<br>
-                        {{$post->author}}<br>
-                        {{$post->content}}<br>                        
-                        {{$post->created_at->diffForHumans()}}
+                            {{$post->file_path}}<br>                        
+                            {{$post->category}}<br>
+                            {{$post->author}}<br>
+                            {{$post->content}}<br>                        
+                            {{$post->created_at->diffForHumans()}}
+                        </li>
+                        <li>
+                            <form action="/post/{{$post->id}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button id="showpost" class="inline-flex disabled items-center px-4 py-2 mx-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">Delete</button>
+                            </form>
                         </li>
                         @endforeach
-                        </ul>
+                    </ul>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="py-12">
+    <h2>Review/delete subs: </h2>
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                <div id="showSubs">
+                    <ul>
+                        @foreach ($subs as $sub)
+                        <li><b>{{$sub->title}}:</b></li>
+                        <li>
+                            {{$sub->file_path}}<br>                        
+                            {{$sub->category}}<br>
+                            {{$sub->author}}<br>
+                            {{$sub->content}}<br>                        
+                            {{$sub->created_at->diffForHumans()}}
+                        </li>
+                        <li>
+                            <form action="/subs/{{$sub->id}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button id="showsubs" class="inline-flex disabled items-center px-4 py-2 mx-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">Delete</button>
+                            </form>
+                        </li>
+                        @endforeach
+                    </ul>
                 </div>
                 
             </div>
@@ -73,10 +112,10 @@ function checkFields () {
         }
     });
 
-    if (countFields =< 4) {                 // If counter reaches 4 display button
-        create.style.display = "block"
+    if (countFields == 4) {                 // If counter reaches 4 display button
+        newpost.style.display = "block"
     } else {                                // If counter is NOT 5 then button doesnt appear
-        create.style.display = "none"
+        newpost.style.display = "none"
     }
 
 }
